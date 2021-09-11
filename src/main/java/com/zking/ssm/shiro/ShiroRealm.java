f9cd61933e5a75ac28b1c18234204720b3d439d9
ArHeng <964312116@qq.com>
2021-09-11 20:51:00 +0800

    权限讲解

---
+++ src/main/java/com/zking/ssm/shiro/ShiroRealm.java
@@ -0,0 +1,56 @@
+package com.zking.ssm.shiro;
+
+import com.zking.ssm.model.User;
+import com.zking.ssm.service.IUserService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class ShiroRealm extends AuthorizingRealm {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String userName = principalCollection.getPrimaryPrincipal().toString();
+
+        //验证用户角色（获取用户名）
+        Set<String> roles = userService.getRole(userName);
+        //获取用户权限
+        Set<String> permissions = userService.getPermission(userName);
+
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        info.setRoles(roles);
+        info.setStringPermissions(permissions);
+        return info;
+    }
+
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        String username=authenticationToken.getPrincipal().toString();
+        String password = authenticationToken.getCredentials().toString();
+        User user=new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        User u = userService.login(user);
+        if(null==u){
+            throw new RuntimeException("账号不存在");
+        }
+        SimpleAuthenticationInfo simpleAuthenticationInfo=new SimpleAuthenticationInfo(u.getUsername(),u.getPassword(), ByteSource.Util.bytes(u.getSalt()),this.getName());
+
+
+        return simpleAuthenticationInfo;
+    }
+}
---
+++ target/ssmshiro/WEB-INF/jsp/delete.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: T440s
+  Date: 2021/9/10
+  Time: 11:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form action="/ssmshiro/handleUser/delete" method="post">
+        编号：<input type="text" name="uid"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
